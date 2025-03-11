182dd7ea1a4fe533f88199952288b1a603609cab
Himani.Vaidya <85447801+hvaidya14@users.noreply.github.com>
2025-03-11 16:54:39 -0700

    Update sample.java

--- sample.java
+++ sample.java
@@ -5,3 +5,23 @@
 
 
 // Your code here along with comments explaining your approach
+class Solution {
+    public List<Integer> partitionLabels(String s) {
+        List<Integer> result = new ArrayList<>();
+        Map<Character, Integer> m = new HashMap<>();
+        int start=0;
+        int end=0;
+        for (int i=0;i<s.length();i++) {
+            m.put(s.charAt(i), i);
+        }
+        for(int i=0;i<s.length();i++) {
+            end = Math.max(end, m.get(s.charAt(i)));
+            if (i == end) {
+                result.add(end-start+1);
+                start=i+1;
+            }
+        }
+
+        return result;
+    }
+}
