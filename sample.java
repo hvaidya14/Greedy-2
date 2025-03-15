bcd9506f3d70e6e2d6d5ab1b473d19f070afb32d
Himani.Vaidya <85447801+hvaidya14@users.noreply.github.com>
2025-03-15 10:45:11 -0700

    Update sample.java

--- sample.java
+++ sample.java
@@ -57,3 +57,22 @@ class Solution {
         return time;
     }
 }
+
+
+class Solution {
+    public int[][] reconstructQueue(int[][] people) {
+        Arrays.sort(people, (a,b) -> {
+            if(a[0] == b[0]) {
+                return a[1] - b[1];
+            } else {
+                return b[0] - a[0];
+            }
+    });
+        List<int[]> q = new LinkedList<>();
+        for(int[] person:people) {
+            q.add(person[1], person);
+        }
+
+        return q.toArray(new int[][] {});
+    }
+}
